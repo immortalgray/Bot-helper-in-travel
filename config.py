1132a2ac057ffce318b1b72c82f46dc8877dafef
Sanchous2363 <151240556+Sanchous2363@users.noreply.github.com>
2024-05-26 12:49:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 LOGS = 'logs.txt'  # файл для логов
 
-IAM_TOKEN = "t1.9euelZqNjJvOzInGnZXNks2KkZzPm-3rnpWanJSKkMjHis3Ijc-ZiY7LjJ7l8_ceIjlN-e98c1Zj_d3z915QNk3573xzVmP9zef1656VmpjHj4yYm5yXns2Kio3IiYqS7_zF656VmpjHj4yYm5yXns2Kio3IiYqSveuelZrJkY6ZiZOXysqRyJTLipDPzbXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.REKS6Rqvnxal0-7NZ63Chy8TTlfw7XpUB5WDYFHxj9g9MuM4oBzOkiwujUvj01lF-beCeGxmpJKccTzET7iRBg"
-FOLDER_ID = "b1g7scu9jilatp911rei"
-API_KEY = "d1ef3b03ce19cb4a66cc5b125faa5ecb"
-BOT_TOKEN = "7163123182:AAGnClmzVDMngdRB-T4ta4uCkoLceDx5WPQ"
+IAM_TOKEN = "eCeGxmpJKccTzET7iRBg"
+FOLDER_ID = "bei"
+API_KEY = "d1aa5ecb"
+BOT_TOKEN = "7163123nClmzVDMnx5WPQ"
 MAX_GPT_TOKENS = 150
 MAX_TOKENS_FOR_PEOPLE = 10000
 DESCRIPTION_ATTRACTION = [{'role': 'system', 'text':  "Ты знаешь о интересных местах, памятниках и тд. Можешь кратко интересно рассказать о любом, сейчас нужно рассказать о"}]
